0ed183fd61f9e6b44edcaf5ad54666a6813fd032
dike-okayama <lawliet.d.k.m@gmail.com>
2023-11-13 04:09:47 +0900

    Refactor buttons in Home to use Button component

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,21 @@ export default function Home() {
   return (
     <main className={styles.homeRoot}>
       <div className={styles.buttonContainer}>
-        <div className={styles.buttonWrapper}>
-          <Link href="/play" draggable={false}>
-            <ArcadeButton />
-          </Link>
-          PLAY
-        </div>
-        <div className={styles.buttonWrapper}>
-          <Link href="/train" draggable={false}>
-            <ArcadeButton />
-          </Link>
-          TRAIN
-        </div>
-        <div className={styles.buttonWrapper}>
-          <Link href="/config" draggable={false}>
-            <ArcadeButton />
-          </Link>
-          CONFIG
-        </div>
+        <Button href="/play" label="PLAY" />
+        <Button href="/train" label="TRAIN" />
+        <Button href="/config" label="CONFIG" />
       </div>
     </main>
   );
 }
+
+const Button = ({ href, label }: { href: string; label: string }) => {
+  return (
+    <div className={styles.buttonWrapper}>
+      <Link href={href} draggable={false}>
+        <ArcadeButton />
+      </Link>
+      {label}
+    </div>
+  );
+};
